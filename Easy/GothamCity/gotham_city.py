330187f6348ba095558e22569df51e65a8d8bd22
Chitrakshi Gosain <chitrakshigosain@gmail.com>
2023-05-22 00:02:46 +1000

    solved easy prob 2

---
+++ Easy/GothamCity/gotham_city.py
@@ -0,0 +1,8 @@
+count_criminals = int(input())
+
+if count_criminals < 5:
+    print('I got this!')
+elif count_criminals > 10:
+    print('Good Luck out there!')
+else:
+    print('Help me Batman')
